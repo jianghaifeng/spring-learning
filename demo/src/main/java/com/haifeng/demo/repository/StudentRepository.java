103367d500245b13471877a8adecccfe3ada3def
Haifeng Jiang <hajian1@ap.xom.com>
2020-02-13 23:48:44 +0800

    add database support

--- demo/src/main/java/com/haifeng/demo/repository/StudentRepository.java
+++ demo/src/main/java/com/haifeng/demo/repository/StudentRepository.java
@@ -4,8 +4,6 @@ import com.haifeng.demo.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.Table;
-
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
