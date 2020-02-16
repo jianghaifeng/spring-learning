f49cde54cd0247fafb7a9332aa83a80ad281882f
Haifeng Jiang <hajian1@ap.xom.com>
2020-02-16 14:38:03 +0800

    config logging

--- demo/src/main/java/com/haifeng/demo/service/StudentService.java
+++ demo/src/main/java/com/haifeng/demo/service/StudentService.java
@@ -2,12 +2,14 @@ package com.haifeng.demo.service;
 
 import com.haifeng.demo.entity.Student;
 import com.haifeng.demo.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 public class StudentService {
 
@@ -15,6 +17,11 @@ public class StudentService {
   StudentRepository studentRepository;
 
   public List<Student> getStudents() {
+
+    log.error("error level: Get all students");
+    log.warn("warn level: Get all students");
+    log.info("info level: Get all students");
+    log.debug("debug level: Get all students");
     return studentRepository.findAll();
   }
 
@@ -22,7 +29,6 @@ public class StudentService {
     List<Student> students = new ArrayList<>();
 //    students.add(new Student("Peter", 28));
 //    students.add(new Student("Jason", 34));
-
     return students;
   }
 }
